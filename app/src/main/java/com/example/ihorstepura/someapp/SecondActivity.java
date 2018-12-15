6fbf664a272c75786bc503d62c8589e7a6c9a68b
ihorstepura <44932530+ihorstepura@users.noreply.github.com>
2018-12-16 00:39:11 +0100

    GitHub test

--- app/src/main/java/com/example/ihorstepura/someapp/SecondActivity.java
+++ app/src/main/java/com/example/ihorstepura/someapp/SecondActivity.java
@@ -3,13 +3,19 @@ package com.example.ihorstepura.someapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private Button button_1;
+    private Button button_2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_second);
+
     }
+
 }
